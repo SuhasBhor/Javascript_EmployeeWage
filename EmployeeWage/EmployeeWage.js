95038ed29cc230cf9b414007d37433774c0b04c5
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-12-30 23:30:04 +0530

    Calculating Wages till Number of Working Days or Total Working Hours per month is Reached

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -4,7 +4,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUMBER_OF_WORKING_DAYS = 20;
+const NUMBER_OF_WORKING_DAYS = 10;
+const MAX_HRS_IN_MONTH = 100;
 
 function getWorkingHours(empCheck){
     switch(empCheck){
@@ -16,8 +17,13 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
+
 let empHrs = 0;
-for(let i = 0; i < NUMBER_OF_WORKING_DAYS; i++){
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let empCheck = (Math.floor(Math.random()*10) % 3);
     empHrs += getWorkingHours(empCheck);
 }
